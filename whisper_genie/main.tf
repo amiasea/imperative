accb90427c571726a23e22483aeec08053716cba
Alfredo Ball <alfredo.ball@outlook.com>
2026-09-04 16:12:19 -0500

    institutive -> imperative

--- whisper_genie/main.tf
+++ whisper_genie/main.tf
@@ -1,9 +1,9 @@
-data "azurerm_key_vault" "institutive_key_vault" {
-    resource_group_name = "rg-amiasea-institutive"
-    name = "kv-amiasea-institutive"
+data "azurerm_key_vault" "imperative_key_vault" {
+    resource_group_name = "rg-amiasea-imperative"
+    name = "kv-amiasea-imperative"
 }
 
 data "azurerm_key_vault_secret" "amiasea_github_app_private_key" {
   name         = "amiasea-github-app-private-key"
-  key_vault_id = data.azurerm_key_vault.institutive_key_vault.id
+  key_vault_id = data.azurerm_key_vault.imperative_key_vault.id
 }
